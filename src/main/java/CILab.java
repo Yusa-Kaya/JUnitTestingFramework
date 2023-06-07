feb470a31b75f47d0d80df4f91e0f0152ace77d2
Yusa-Kaya <ozgur.yusa.kaya@gmail.com>
2023-06-07 12:25:22 -0700

    Implemented detectCapitalUse method and test cases

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,8 +13,35 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        if (myString == null) {
+            throw new IllegalArgumentException("The string cannot be null!");
+        }else if(myString.length() == 0){
+            return false;
+        }
+
+        if(myString.equals(myString.toUpperCase())){
+            return true;
+        }else if(myString.equals(myString.toLowerCase())){
+            return true;
+        }else{
+            // Check if the first character is uppercase
+            if (!Character.isUpperCase(myString.charAt(0))) {
+                return false;
+            }
+
+            // Check if the remaining characters are lowercase
+            for (int i = 1; i < myString.length(); i++) {
+                if (!Character.isLowerCase(myString.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
+    public static void main(String[] args){
+        CILab x = new CILab();
+        System.out.println(x.detectCapitalUse());
+    }
 }
 
--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -20,12 +20,39 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+       myString.setString("Hello");
+       boolean expected = true;
+       boolean actual = myString.detectCapitalUse();
+       assertEquals(expected, actual);
     }
+
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("HELLO");
+        boolean expected = true;
+        boolean actual = myString.detectCapitalUse();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("hello");
+        boolean expected = true;
+        boolean actual = myString.detectCapitalUse();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("");
+        boolean expected = false;
+        boolean actual = myString.detectCapitalUse();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void detectCapitalUseTest5() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            myString.detectCapitalUse();});
     }
 
 
